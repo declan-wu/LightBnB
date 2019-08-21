b5d921df9d33e67a7188d0c83848366e14df223e
Declan Wu <declan.s.wu@gmail.com>
2019-08-20 20:06:46 -0400

    added migration file and seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,49 @@
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Willem Ariss', 'wariss0@google.ca');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Arden Walcot', 'awalcot1@guardian.co.uk');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Georgeanne Saintsbury', 'gsaintsbury2@behance.net');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Felecia Gomersal', 'fgomersal3@businessinsider.com');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Kareem Daldry', 'kdaldry4@flickr.com');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Camey Rennocks', 'crennocks5@mysql.com');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Mahalia Butler-Bowdon', 'mbutlerbowdon6@si.edu');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Antonella Durden', 'adurden7@about.me');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Arnoldo Wones', 'awones8@yellowbook.com');
+insert into users (password, name, email) values ('$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Peggy Bartomeu', 'pbartomeu9@google.com.au');
+
+
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Smallhead Cat''s Ear', 'http://dummyimage.com/245x205.jpg/dddddd/000000', 'http://dummyimage.com/193x128.bmp/5fa2dd/ffffff', 'United States', '75512 Dottie Trail', 'Salt Lake City', 'Utah', '84110');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Sphagnum', 'http://dummyimage.com/150x195.jpg/5fa2dd/ffffff', 'http://dummyimage.com/197x242.png/ff4444/ffffff', 'United States', '1831 Valley Edge Junction', 'Pittsburgh', 'Pennsylvania', '15225');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Lesser Burdock', 'http://dummyimage.com/218x130.bmp/dddddd/000000', 'http://dummyimage.com/131x185.jpg/dddddd/000000', 'United States', '9 Erie Lane', 'Albuquerque', 'New Mexico', '87140');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Pore Lichen', 'http://dummyimage.com/229x238.bmp/5fa2dd/ffffff', 'http://dummyimage.com/117x211.bmp/5fa2dd/ffffff', 'United States', '340 Armistice Street', 'Houston', 'Texas', '77266');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Lindley''s Clerodendrum', 'http://dummyimage.com/195x239.bmp/dddddd/000000', 'http://dummyimage.com/149x150.png/cc0000/ffffff', 'United States', '83 Cascade Lane', 'Louisville', 'Kentucky', '40266');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Wartleaf Ceanothus', 'http://dummyimage.com/154x119.bmp/cc0000/ffffff', 'http://dummyimage.com/250x193.jpg/dddddd/000000', 'United States', '84572 Paget Avenue', 'Salt Lake City', 'Utah', '84189');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Kiwi', 'http://dummyimage.com/134x137.png/ff4444/ffffff', 'http://dummyimage.com/162x190.jpg/dddddd/000000', 'United States', '27 Sunnyside Way', 'Albuquerque', 'New Mexico', '87201');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Woolly Hawthorn', 'http://dummyimage.com/211x142.jpg/ff4444/ffffff', 'http://dummyimage.com/214x166.bmp/ff4444/ffffff', 'United States', '3835 Mayfield Avenue', 'Albuquerque', 'New Mexico', '87105');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Crimson Woodsorrel', 'http://dummyimage.com/112x155.jpg/dddddd/000000', 'http://dummyimage.com/214x223.jpg/dddddd/000000', 'United States', '8 Bluestem Way', 'Norwalk', 'Connecticut', '06854');
+insert into properties (description, title, cover_photo_url, thumbnail_photo_url, country, street, city, province, post_code) values ('description', 'Dot Lichen', 'http://dummyimage.com/247x205.bmp/dddddd/000000', 'http://dummyimage.com/144x100.jpg/ff4444/ffffff', 'United States', '14 2nd Place', 'San Antonio', 'Texas', '78296');
+
+
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (1, 1, '2018-09-11', '2018-09-26');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (2, 2, '2019-01-04', '2019-02-01');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (3, 5, '2021-10-01', '2021-10-14');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (9, 1, '2018-09-11', '2018-09-26');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (2, 5, '2019-01-04', '2019-02-01');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (6, 3, '2021-10-01', '2021-10-14');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (7, 1, '2018-09-11', '2018-09-26');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (2, 4, '2019-01-04', '2019-02-01');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (9, 3, '2021-10-01', '2021-10-14');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (9, 9, '2018-09-11', '2018-09-26');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (4, 2, '2019-01-04', '2019-02-01');
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) VALUES (5, 5, '2021-10-01', '2021-10-14');
+
+
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (1, 2, 1, 8, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (2, 3, 3, 8, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (3, 9, 6, 8, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (4, 2, 7, 1, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (5, 5, 9, 2, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (6, 2, 1, 3, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (7, 6, 2, 9, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (8, 1, 4, 7, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (9, 9, 6, 3, 'TEXT'); 
+INSERT INTO property_reviews (property_id, guest_id, reservation_id, rating, message) VALUES (10, 4, 8, 9, 'TEXT'); 
